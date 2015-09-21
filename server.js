7f21ad2ab37d94d99636d6f6fe2f3050f2198b70
kordano <konrad.kuehne@rocketmail.com>
2015-09-21 11:10:07 +0200

    hack some fortunes

--- server.js
+++ server.js
@@ -2,27 +2,29 @@ var expressio = require('express.io');
 var app = expressio();
 app.http().io();
 
-var sys = require('sys');
-var exec = require('child_process').exec;
-function execute(command, callback){
-    exec(command, function(error, stdout, stderr){ callback(stdout); });
+function tellFortune(callback){
+  var sys = require('sys');
+  var exec = require('child_process').exec;
+  exec("fortune -s -n 40",
+       function(error, stdout, stderr){
+         callback(stdout);
+       });
 };
 
-   
+// Static routes
 app.use(expressio.static('public'));
 app.use('/static', expressio.static('bower_components'));
-
 app.get('/', function(req, res, next) {
   res.sendfile(__dirname + "/public/index.html");
 });
 
+// Websocket routes
 app.io.route('ready', function(req) {
-  execute("fortune -s -n 40",
-          function(fortune) {
-            req.io.emit('fortune', {
-              message: fortune
-            });
-          });
+  tellFortune(function(fortune) {
+    req.io.emit('fortune', {
+      message: fortune
+    });
+  });
 });
 
 var server = app.listen(8080, function () {
@@ -30,3 +32,4 @@ var server = app.listen(8080, function () {
   var port = server.address().port;
   console.log('Server started at: http://%s:%s', host, port);
 });
+
