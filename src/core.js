afe9f3573d12aa763c9ca738b4af8f63658a0e02
kordano <konrad.kuehne@rocketmail.com>
2015-09-21 14:24:16 +0200

    add immutable structures

--- src/core.js
+++ src/core.js
@@ -1,16 +1,23 @@
-var model = Immutable.Seq([{title: 'GI Joe', ts: 1066, tags: ['a', 'b', 'c'], url: "google.com"},
-                           {title: 'GI Jane', ts: 1776 , tags: ['a', 'b', 'c'], url: "google.com"},
-                           {title: 'GI Konny', ts: 666, tags: ['a', 'b', 'c'], url: "google"}]);
-var uri = "localhost:8080";
+"use strict"
+var model = {bookmarks: [{title: 'GI Joe', ts: 1066, tags: ['a', 'b', 'c'], url: "google.com"},
+                         {title: 'GI Jane', ts: 1776 , tags: ['a', 'b', 'c'], url: "google.com"},
+                         {title: 'GI Konny', ts: 666, tags: ['a', 'b', 'c'], url: "google"}],
+             uri: "localhost:8080"}
 
-socket = io.connect();
-socket.on('connect', function() {
-  console.log("Connected to " + uri);
-});
-socket.on('disconnect', function(){
-  console.log("Disconnected from " + uri);
-});
-    
+function initialConnection(uri){
+ var socket = io.connect()
+  socket.on('connect', function() {
+    console.log("Connected to " + uri)
+  })
+  socket.on('error', function() {
+    console.log("ERROR on SOCKET" + uri)
+  })
+  socket.on('disconnect', function(){
+    console.log("Disconnected from " + uri)
+  })
+  return socket
+}
+   
 var bookmark = React.createClass({
   render : function() {
     var tags = this.props.tags.map(function (t) {
@@ -22,54 +29,67 @@ var bookmark = React.createClass({
     var tagList = React.DOM.ul(null, tags);
     return React.DOM.li(null, title, ts, tagList);
   }
-});
+})
 
 var bookmarkList = React.createClass({
   render : function() {
     var title = React.DOM.h1(null, "Bookmarks");
-    var bookmarks = this.props.data.map(function (b) {
+    var bookmarks = this.props.data.bookmarks.map(function (b) {
       return React.createElement(bookmark,
                                  {url: b.url,
                                   title: b.title,
                                   tags: b.tags,
-                                  ts: b.ts});
-    });
-    return React.DOM.ol(null, title, bookmarks);
+                                  ts: b.ts})
+    })
+    return React.DOM.ol(null, title, bookmarks)
   }
-});
+})
 
 var bookmarkInput = React.createClass({
+  getInitialState : function() {
+    return {
+      data: Immutable.Map({searchText: ""})
+    }
+  },
+  handleChange: function(event) {
+    this.setState(({data}) => ({
+      data: data.update('searchText', v => v + (event.target ? event.target.value : ""))
+    }))
+  },
   render : function () {
-    return React.DOM.input({value: this.props.searchText, onChange: this.props.handleChange});
+    return React.DOM.input({value: this.searchText, onChange: this.handleChange})
   }
-});
+})
 
 var overseer = React.createClass({
   getInitialState : function() {
-    return {searchText: ""};
-  },
-  handleChange: function(event) {
-    this.setState({searchText: event.target.value});
+    return {
+      data: Immutable.Map({welcomeText: ""})
+    }
   },
   componentDidMount : function() {
-    socket.emit('ready');
-    socket.on('fortune', function(data) {
-      console.log(data.message);
-    });
+    var socket = initialConnection(this.props.data.uri)
+    socket.emit('ready')
+    socket.on('fortune', function(msg) {
+        if(this.isMounted()) {
+          this.setState(({data}) => ({
+            data: data.update('welcomeText', v => msg)
+          }))
+        }
+    }.bind(this))
   },
   render : function() {
-    var bl = React.createElement(bookmarkList, {data: this.props.data});
-    var st = this.state.searchText;
-    var iF = React.createElement(bookmarkInput, {searchText: st, handleChange: this.handleChange});
-    return React.DOM.div(null, iF, bl);
+    var bookmarks = React.createElement(bookmarkList, {data: this.props.data})
+    var inputField = React.createElement(bookmarkInput, null)
+    var welcomeMessage = React.DOM.h3(null, this.state.data.get('welcomeText'))
+    return React.DOM.div(null, welcomeMessage, inputField, bookmarks)
   }
-});
+})
 
 function createRoot() {
-  return React.createElement(overseer, {data: model});
-};
+  return React.createElement(overseer, {data: model})
+}
 
-
-var root = createRoot();
-var container = document.body;
-React.render(root, container);
+var root = createRoot()
+var container = document.body
+React.render(root, container)
